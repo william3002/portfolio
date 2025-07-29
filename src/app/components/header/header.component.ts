ab4a229f8f289bd80ccf1e78bf7955dd01e8b739
William Araujo <106315401+william3002@users.noreply.github.com>
2025-07-29 13:14:50 -0300

    fix: adicionado CommonModule ao módulo para corrigir erro de binding

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ RouterLink, RouterLinkActive],
+  imports: [ CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
